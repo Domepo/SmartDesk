5701bd38d381ed0f3a953f3b0db8bf44b1315210
Domepo <36802235+Domepo@users.noreply.github.com>
2020-07-14 09:57:13 +0200

    Update index_new.js

--- test/htmlFormBox/index_new.js
+++ test/htmlFormBox/index_new.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let can_frontend = document.createElement("div");
-
+let count = 0;
 window.addEventListener("load",function(){
 
     function create_radio_box(){
@@ -53,8 +53,10 @@ window.addEventListener("load",function(){
                 const removeClass = (elms) => elms.forEach(el => el.remove());
                 removeClass(document.querySelectorAll(".master-id-class") );
                 removeClass(document.querySelectorAll(".filter_div_style") );
+                removeClass(document.querySelectorAll(".ok") );
 
                 callback();
+                
             }
             if (document.getElementById("radio-recieve-button-id").checked == true) {
 
@@ -65,12 +67,17 @@ window.addEventListener("load",function(){
                 const removeClass = (elms) => elms.forEach(el => el.remove());
                 removeClass(document.querySelectorAll(".master-id-class") );
                 removeClass(document.querySelectorAll(".filter_div_style") );
+                removeClass(document.querySelectorAll(".ok") );
+
             }
         
         });
     }
-    function create_id_name_master_field(){
+    function create_id_name_master_field(counter){
 
+        if(counter == undefined){
+            counter = 0;
+        }
 
         let field_for_id_and_master = document.createElement("div"),
             master_id_field         = document.createElement("input"),
@@ -79,28 +86,30 @@ window.addEventListener("load",function(){
             label_for_name_field    = document.createElement("label"),
             n_filter                = document.createElement("input"),
             label_for_n_filter      = document.createElement("label"),
-            ready_button            = document.createElement("button");
+            ready_button            = document.createElement("button"),
+            bracket                 = document.createElement("br");
 
-
-        field_for_id_and_master.setAttribute("class","master-id-class");    
-        field_for_id_and_master.setAttribute("id","master-id-id");   
         
-        label_for_id_field.setAttribute("for","master_id_field");
-        label_for_name_field.setAttribute("for","master_name_field");
+        field_for_id_and_master.setAttribute("class","master-id-class"+counter);    
+        field_for_id_and_master.setAttribute("id","master-id-id"+counter);   
+        
+        label_for_id_field.setAttribute("for","master_id_field"+counter);
+        label_for_name_field.setAttribute("for","master_name_field"+counter);
 
-        master_name_field.setAttribute("id","master-name-field");
-        master_id_field.setAttribute("id","master-id-field");
+        master_name_field.setAttribute("id","master-name-field"+counter);
+        master_id_field.setAttribute("id","master-id-field"+counter);
 
         n_filter.setAttribute("type","number");
         n_filter.setAttribute("class","n_filter_style")
-        n_filter.setAttribute("id","n-filter");
+        n_filter.setAttribute("id","n-filter"+counter);
 
         label_for_n_filter.setAttribute("for","n_filter");
 
         ready_button.setAttribute("id","ready-button");
 
         can_frontend.appendChild(field_for_id_and_master);
-    
+        
+        field_for_id_and_master.appendChild(bracket);
         field_for_id_and_master.appendChild(label_for_id_field);
         field_for_id_and_master.appendChild(master_id_field);
         field_for_id_and_master.appendChild(label_for_name_field);
@@ -120,15 +129,50 @@ window.addEventListener("load",function(){
 
             const removeClass = (elms) => elms.forEach(el => el.remove());
             removeClass(document.querySelectorAll(".filter_div_style") );
+            removeClass(document.querySelectorAll(".ok") );
 
-            let get_n_filter_value = document.getElementById("n-filter").value;
+            let get_n_filter_value = document.getElementById("n-filter"+counter).value;
+
             for(let i=0;i<get_n_filter_value;i++){
-                create_filter_box(get_n_filter_value,i,i,i);
+                create_filter_box(get_n_filter_value,i,i,i,counter);
             }
+            //if there is more than one object
+
+            add_obj();
+
         });
+
     }
 
-    function create_filter_box(value,div_n,name_n,id_n){
+    function add_obj(){
+
+        let add_border                  = document.createElement("div"),
+        add_button                      = document.createElement("button");
+
+        add_button.setAttribute("class","ok");
+        add_button.setAttribute("id","jaw");
+
+        can_frontend.appendChild(add_border);
+        add_border.appendChild(add_button);
+
+        add_button.innerHTML = "More";
+
+        
+        let master_more_button = document.getElementById("jaw");
+        master_more_button.addEventListener("click",()=>{
+            count++;
+            create_id_name_master_field(count);
+
+            
+        });
+
+    }
+
+    function create_filter_box(value,div_n,name_n,id_n,master_identifier){
+        
+        if(master_identifier == undefined){
+            master_identifier = 0;
+        }
 
         let filter_div                  = document.createElement("div"),
         filter_box_name                 = document.createElement("input"),
@@ -136,14 +180,14 @@ window.addEventListener("load",function(){
         label_for_filter_name           = document.createElement("label"),
         label_for_filter_id             = document.createElement("label");
 
-        label_for_filter_id.setAttribute("for","filter_box_id");
-        label_for_filter_name.setAttribute("for","filter_box_name");
+        label_for_filter_id.setAttribute("for",master_identifier+"filter_box_id");
+        label_for_filter_name.setAttribute("for",master_identifier+"filter_box_name");
 
-        filter_div.setAttribute("class","filter_div_style");
-        filter_div.setAttribute("id","filter_div_id"+div_n);
+        filter_div.setAttribute("class",master_identifier+"filter_div_style");
+        filter_div.setAttribute("id",master_identifier+"filter_div_id"+div_n);
 
-        filter_box_name.setAttribute("id","box_name"+name_n);
-        filter_box_id.setAttribute("id","id_name"+id_n);
+        filter_box_name.setAttribute("id",master_identifier+"box_name"+name_n);
+        filter_box_id.setAttribute("id",master_identifier+"id_name"+id_n);
 
 
         can_frontend.appendChild(filter_div);
@@ -160,9 +204,5 @@ window.addEventListener("load",function(){
 
     create_radio_box();
     get_radio_data(create_id_name_master_field);
-
-
-
-
 
 }); 
