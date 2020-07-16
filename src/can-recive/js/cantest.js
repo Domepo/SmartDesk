9f88c985ed18e753f7cde32e8997b7247cbc2770
Domepo <36802235+Domepo@users.noreply.github.com>
2020-07-16 13:20:53 +0200

    Update cantest.js

--- src/can-recive/js/cantest.js
+++ src/can-recive/js/cantest.js
@@ -1,18 +1,24 @@
-
 var can = require('socketcan');
 //our canbus is the can0
 var channel = can.createRawChannel("can0", true);
 
 // Log all data [8bit]
-channel.addListener("onMessage", function(msg) { console.log(msg.data);
-
+channel.addListener("onMessage", function(msg) {
+  
 const buf1 = Buffer.from(msg.data);
 
 let json = buf1.toJSON();
 
-console.log(json['data']);});
+let djson = {
+    "id": 123,
+    "msg": 1
+};
+djson.id = msg.id;
+djson.msg = json["data"];
+console.log(djson)
+
+;});
 // Log everything | uncomment this and comment the code above
 //channel.addListener("onMessage", function(msg) { console.log(msg); } );
 
 channel.start();
-
