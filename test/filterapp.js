6f8bd2c52d5c569dbe2b1dbba987b27542b604a2
Domepo <36802235+Domepo@users.noreply.github.com>
2020-07-16 10:48:01 +0200

    Create filterapp.js

---
+++ test/filterapp.js
@@ -0,0 +1,22 @@
+// Filter app
+let arr = [1,2,3,5,6,78,1,25];
+
+let test = {
+    id112 :[10,2]
+};
+
+for(let i = 0;i<arr.length;i++){
+    let p = arr[i];
+
+    if(test.id112.includes(p) == true){
+        
+    }
+    else{
+        test.id112.push(p);
+    }
+
+
+}
+console.log(test);
+
+
